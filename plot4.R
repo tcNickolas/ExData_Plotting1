5e5d0f4534a411205d660e654ffbe2f571f07122
Mariia Mykhailova <michaylova@gmail.com>
2015-01-10 17:11:58 -0800

    Plots and scripts for Course Project 1

---
+++ plot4.R
@@ -0,0 +1,29 @@
+# I assume that the data file is already downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# and extracted to working directory\household_power_consumption.txt
+
+# We only use data from the dates 2007-02-01 and 2007-02-02, represented as 1/2/2007 and 2/2/2007 in the data set
+# they are stored on lines 66638 - 69517, inclusive, so we can read these lines only
+
+data <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = FALSE, skip = 66637, nrows = 2880, col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")); 
+
+data$DateTime <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S");
+
+png(filename = "plot4.png", width = 480, height = 480, bg = "transparent");
+
+par(mfrow = c(2,2));
+plot(data$DateTime, data$Global_active_power, main = "", xlab = "", ylab = "Global Active Power", type="n");
+lines(data$DateTime, data$Global_active_power);
+
+plot(data$DateTime, data$Voltage, main = "", xlab = "datetime", ylab = "Voltage", type="n");
+lines(data$DateTime, data$Voltage);
+
+plot(data$DateTime, data$Sub_metering_1, main = "", xlab = "", ylab = "Energy sub metering", type="n");
+lines(data$DateTime, data$Sub_metering_1, col="black");
+lines(data$DateTime, data$Sub_metering_2, col="red");
+lines(data$DateTime, data$Sub_metering_3, col="blue");
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty=c(1,1), bty="n");
+
+plot(data$DateTime, data$Global_reactive_power, main = "", xlab = "datetime", ylab = "Global_reactive_power", type="n");
+lines(data$DateTime, data$Global_reactive_power);
+
+dev.off();
